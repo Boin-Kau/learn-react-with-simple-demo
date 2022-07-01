0b76c1892fea9deaae9f486c8263f0b956aaacbf
Boin Kim <charles1236@kau.kr>
2022-07-02 02:26:02 +0900

    feat: 2주차 실습 코드 최초 완성

---
+++ react-hooks-demo/README.md
@@ -0,0 +1,3 @@
+# 2주차 실습
+
+<img src="../img/demo2-img1.png" />
---
+++ react-hooks-demo/src/App.js
@@ -0,0 +1,11 @@
+import Register from "./Register";
+
+function App() {
+  return (
+    <div>
+      <Register />
+    </div>
+  );
+}
+
+export default App;
