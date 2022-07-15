cbe717fcf0acdf26fda9a347ce7f4a994be0278c
Boin Kim <charles1236@kau.kr>
2022-07-15 20:34:57 +0900

    feat: 4주차 스터디 실습 코드 추가

---
+++ react-css-tech-demo/src/App.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import RadioGroup from "./RadioGroup";
+
+function App() {
+  return (
+    <div>
+      <RadioGroup />
+    </div>
+  );
+}
+
+export default App;
